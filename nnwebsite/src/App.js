5faed50924e2e50cfb11cd57fb3cfd1458e9cf98
jacobadam <jacobanevitt@gmail.com>
2020-02-10 14:59:41 +0000

    file structure created

--- nnwebsite/src/App.js
+++ nnwebsite/src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>NN Website</h1>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import { Router } from "@reach/router";
+import Header from "./Header";
+import Footer from "./Footer";
+import Homepage from "./Homepage";
+import About from "./About";
+import Scope from "./Scope";
+import Contact from "./Contact";
+import Location from "./Location";
+import Links from "./Links";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>NN Website</h1>
+            <Header/>
+          <Router>
+            <Homepage path="/"/>
+            <About path="/about"/>
+            <Scope path="/scope"/>
+            <Contact path="/contact"/>
+            <Location path="/location"/>
+            <Links path="/links"/>
+          </Router>
+            <Footer/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ nnwebsite/src/Scope.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Scope extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Scope;
